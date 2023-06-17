ba2730ac5fa07ec8a444980e7a05193afa3158bf
Danylo Kuruch <kuruch1004@gmail.com>
2023-06-17 21:54:47 +0200

    v2.0

--- src/forecasting.py
+++ src/forecasting.py
@@ -1,164 +1,353 @@
-import pandas as pd
-import numpy as np
-from statsmodels.tsa.seasonal import STL
-import tkinter
+import sys
+import logging
 from tkinter import filedialog
-import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
+import pandas as pd
 from pandas.plotting import register_matplotlib_converters
-
-#File reading
-file_path_string = filedialog.askopenfilename()
-df = pd.read_csv(file_path_string, usecols=['NA_Sales','EU_Sales','JP_Sales','Other_Sales'])
-print(df)
-arr=df.values
-n=0 #time series number: 0 -'NA_Sales',1 - 'EU_Sales', 2 - 'JP_Sales', 3 -'Other_Sales'
-
-#Plot an initial time series
-fig = plt.figure()
-plt.plot(arr[:200,n], color='red', linewidth=2);
-plt.show()
-
-register_matplotlib_converters()
-sns.set_style("darkgrid")
-plt.rc("figure", figsize=(16, 12))
-plt.rc("font", size=13)
-
-#Time series component plotting function
-def add_stl_plot(fig, res, legend):
-    """Add 3 plots from a second STL fit"""
-    axs = fig.get_axes()
-    comps = ["trend", "seasonal", "resid"]
-    for ax, comp in zip(axs[1:], comps):
-        series = getattr(res, comp)
-        if comp == "resid":
-            ax.plot(series, marker="o", linestyle="none")
-        else:
-            ax.plot(series)
-            if comp == "trend":
-                ax.legend(legend, frameon=False)
-                
-#Defining MAPE function
-def MAPE(Y_actual,Y_Predicted):
-    numerator =np.zeros((len(Y_actual)))
-    for u in range(len(Y_actual)):
-        if Y_actual[u]==0:
-           numerator[u] = (Y_Predicted[u]-Y_actual[u])
-        else:
-           numerator[u] = (Y_Predicted[u]-Y_actual[u])/Y_actual[u]
-    mape = np.median(np.abs(numerator))*100
-    return mape
-
-#Defining U2 function
-def u2(Y_actual,Y_Predicted):
-    Y_Predicted_1 =Y_Predicted[1:]
-    Y_actual_1=Y_actual[1:]
-   
-    numerator =np.zeros((len(Y_actual)))
-    for u in range(len(Y_actual)-1):
-        if Y_actual[u]==0:
-           numerator[u] = (Y_Predicted[u+1]-Y_actual[u+1])
-        else:
-            numerator[u] = (Y_Predicted[u+1]-Y_actual[u+1])/Y_actual[u]
-    denominator =np.zeros((len(Y_actual)))
-    for u in range(len(Y_actual)-1):
-        if Y_actual[u]==0:
-           denominator[u] = (Y_actual[u+1]-Y_actual[u])
-        else:
-            denominator[u] = (Y_actual[u+1]-Y_actual[u])/Y_actual[u]
-    
-    return np.sqrt(np.sum(np.square(numerator))/np.sum(np.square(denominator)))
-
-#Defining U1 function
-def u1(Y_actual,Y_Predicted):
-    
-    numerator =np.zeros((len(Y_actual)))
-    for u in range(len(Y_actual)):
-        numerator[u] = (Y_Predicted[u]-Y_actual[u])
-    numerator = np.sqrt(np.median(np.square(numerator)))
-      
-    return numerator/(np.sqrt(np.median(np.square(Y_Predicted)))+np.sqrt(np.median(np.square(Y_actual))))
-
-#Testing stationarity
+from scipy import stats
+from matplotlib import pyplot as plt
+from statsmodels.tsa.seasonal import STL
 from statsmodels.tsa.stattools import adfuller
-print(adfuller(arr[:300,n]))
-
-#Testing normality
-import scipy.stats as stats
-print(stats.jarque_bera(arr[:300,n]))
-
-#Moving average and std plotting function
-def get_stationarity(timeseries, window):
-    
-    # rolling statistics
-    rolling_mean=np.zeros((300,1))
-    rolling_std=np.zeros((300,1))
-    for u in range(300-window+1):
-        rolling_mean[u] = timeseries[u:(u+window-1)][:].mean()
-        rolling_std[u] = timeseries[u:(u+window-1)][:].std()
-    
-    # rolling statistics plot
-    fig = plt.figure()
-    original = plt.plot(timeseries, color='blue', label='Original')
-    mean = plt.plot(rolling_mean, color='red', label='Mean')
-    std = plt.plot(rolling_std, color='black', label='Std')
-    plt.legend(loc='best')
-    plt.title('Mean & Standard Deviation')
-    plt.show(block=False)
-    
-#Moving average and std plotting
-get_stationarity(arr[:300,n],12)#12- window size
-
-#STL decomposition (robust, nonrobust)                                                                       
-stl = STL(np.log(arr[:200,n]+1), period=3, robust=True)
-res_robust = stl.fit()
-fig = res_robust.plot()
-res_non_robust = STL(np.log(arr[:200,n]+1), period=3, robust=False).fit()
-add_stl_plot(fig, res_non_robust, ["Robust", "Non-robust"])
-plt.show()
-
-print("Non-robust trend MAPE",MAPE(np.log(arr[:200,n]+1),res_non_robust.trend))
-print("Non-robust trend U1", u1(np.log(arr[:200,n]+1),res_non_robust.trend))
-print("Non-robust trend U2", u2(np.log(arr[:200,n]+1),res_non_robust.trend))
-
-print("Non-robust trend and seasonality MAPE",MAPE(np.log(arr[:200,n]+1),res_non_robust.trend+res_non_robust.seasonal))
-print("Non-robust trend and seasonality U1", u1(np.log(arr[:200,n]+1),res_non_robust.trend+res_non_robust.seasonal))
-print("Non-robust trend and seasonality U2", u2(np.log(arr[:200,n]+1),res_non_robust.trend+res_non_robust.seasonal))
-
-print("Robust trend MAPE",MAPE(np.log(arr[:200,n]+1),res_robust.trend))
-print("Robust trend U1", u1(np.log(arr[:200,n]+1),res_robust.trend))
-print("Robust trend U2", u2(np.log(arr[:200,n]+1),res_robust.trend))
-
-print("Robust trend and seasonality MAPE",MAPE(np.log(arr[:200,n]+1),res_robust.trend+res_robust.seasonal))
-print("Robust trend and seasonality U1", u1(np.log(arr[:200,n]+1),res_robust.trend+res_robust.seasonal))
-print("Robust trend and seasonality U2", u2(np.log(arr[:200,n]+1),res_robust.trend+res_robust.seasonal))
-
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.forecasting.stl import STLForecast
 
-#STL forecasting nonrobust
-stlf = STLForecast(np.log(arr[:200,n]+1), ARIMA, model_kwargs=dict(order=(2, 1, 0)), period=5, robust=False)
-stlf_res = stlf.fit()
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+log = logging.getLogger(__name__)
 
-T=6
-forecast = stlf_res.forecast(T)
-plt.plot(np.log(arr[:200+T,n]+1))
-plt.plot(range(200,200+T),forecast,color='red')
-plt.show()
 
-print("Forecasting nonrobust MAPE", MAPE(np.log(arr[200:200+T,n]+1),forecast))
-print("Forecasting nonrobust U1",u1(np.log(arr[200:200+T,n]+1),forecast))
-print("Forecasting nonrobust U2",u2(np.log(arr[200:200+T,n]+1),forecast))
+def log_separator():
+    """Prints a separator in the log."""
+    log.info("\n")
 
-#STL forecasting robust
-stlf = STLForecast(np.log(arr[:200,n]+1), ARIMA, model_kwargs=dict(order=(2, 1, 0)), period=5, robust=True)
-stlf_res = stlf.fit()
 
-forecast = stlf_res.forecast(T)
-plt.plot(np.log(arr[:200+T,n]+1))
-plt.plot(range(200,200+T),forecast,color='red')
-plt.show()
+def get_cmd_args():
+    """
+    Collects command arguments in one dictionary object
+    
+    Returns:
+        dict: a dictionary object, which includes command arguments
+    """
+    if len(sys.argv) > 1:
+        argv = sys.argv[1:]
+        args_dict = {}
+        for i in range(0, len(argv), 2):
+            args_dict[argv[i]] = argv[i + 1]
+        return args_dict
+    return {}
 
-print("Forecasting robust MAPE", MAPE(np.log(arr[200:200+T,n]+1),forecast))
-print("Forecasting robust U1",u1(np.log(arr[200:200+T,n]+1),forecast))
-print("Forecasting robust U2",u2(np.log(arr[200:200+T,n]+1),forecast))
+
+def get_or_default(args, field, default):
+    """
+    Takes from "args" a necessary field or returns a default value
+
+    Args:
+        args (dict): a dictionary of command args
+        field (string): a command argument
+        default (any): a value, which returns, if "args" parameter does not have a necessary field
+
+    Returns:
+        any: value from "args" object of "field" key
+    """
+    if field in args:
+        return args[field]
+    return default
+
+
+def read_csv_file(file_path, columns):
+    """
+    Reads a CSV file and returns a DataFrame.
+
+    Args:
+        columns: List of column names to read from the CSV file.
+
+    Returns:
+        The DataFrame containing the data read from the CSV file.
+    """
+    try:
+        if not file_path:
+            file_path = filedialog.askopenfilename()
+        return pd.read_csv(file_path, usecols=columns)
+    except FileNotFoundError:
+        log.error("File is not found.")
+        sys.exit()
+
+
+def mape(y_actual, y_predicted):
+    """
+    Calculates the Mean Absolute Percentage Error (MAPE).
+
+    Args:
+        y_actual: Array-like object of actual values.
+        y_predicted: Array-like object of predicted values.
+
+    Returns:
+        The MAPE value.
+    """
+    numerator = np.where(
+        y_actual == 0, y_predicted - y_actual, (y_predicted - y_actual) / y_actual
+    )
+    return np.median(np.abs(numerator)) * 100
+
+
+def u_1(y_actual, y_predicted):
+    """
+    Calculates the U1 metric.
+
+    Args:
+        y_actual: Array-like object of actual values.
+        y_predicted: Array-like object of predicted values.
+
+    Returns:
+        The U1 value.
+    """
+    numerator = y_predicted - y_actual
+    numerator = np.sqrt(np.median(np.square(numerator)))
+
+    denominator = np.sqrt(np.median(np.square(y_predicted))) + np.sqrt(
+        np.median(np.square(y_actual))
+    )
+
+    return numerator / denominator
+
+
+def u_2(y_actual, y_predicted):
+    """
+    Calculates the U2 metric.
+
+    Args:
+        y_actual: Array-like object of actual values.
+        y_predicted: Array-like object of predicted values.
+
+    Returns:
+        The U2 value.
+    """
+    numerator = np.where(
+        y_actual[:-1] == 0,
+        y_predicted[1:] - y_actual[1:],
+        (y_predicted[1:] - y_actual[1:]) / y_actual[:-1],
+    )
+    denominator = np.where(
+        y_actual[:-1] == 0,
+        y_actual[1:] - y_actual[:-1],
+        (y_actual[1:] - y_actual[:-1]) / y_actual[:-1],
+    )
+
+    return np.sqrt(np.sum(np.square(numerator)) / np.sum(np.square(denominator)))
+
+
+def check_stationarity(time_serie, window_size):
+    """
+    Computes and plots the rolling mean and standard deviation to assess stationarity.
+
+    Args:
+        time_series: Array-like object representing the time series data.
+        window_size: Size of the rolling window for computing the mean and standard deviation.
+    """
+    rolling_mean = np.convolve(
+        time_serie, np.ones(window_size) / window_size, mode="valid"
+    )
+    rolling_std = np.std(
+        np.lib.stride_tricks.sliding_window_view(time_serie, window_size), axis=1
+    )
+
+    plt.figure(label="Assess Stationarity")
+    plt.plot(time_serie, color="blue", label="Original")
+    plt.plot(
+        np.pad(rolling_mean, (window_size - 1, 0), mode="constant"),
+        color="red",
+        label="Mean",
+    )
+    plt.plot(
+        np.pad(rolling_std, (window_size - 1, 0), mode="constant"),
+        color="black",
+        label="Std",
+    )
+    plt.legend(loc="best")
+    plt.title("Mean & Standard Deviation")
+    plt.show(block=False)
+
+
+def build_time_series(data_frame, length, index, name):
+    """
+    Builds and plots a time series.
+
+    Args:
+        data_frame: DataFrame containing the data.
+        time_series_length: The length of the processing time series data.
+        time_series_index: Index of the specific time series column in the DataFrame.
+        name: Name of the time series.
+    """
+    plt.figure(label="Time Series Plot", figsize=(10, 6))
+    plt.plot(
+        data_frame.values[:length, index],
+        color="red",
+        linewidth=2,
+        label=name,
+    )
+
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.title("Time Series")
+    plt.legend()
+
+    log.info("Data frame values:\n%s", data_frame)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def perform_stl_decomposition(time_serie, length, index):
+    """
+    Performs STL (Seasonal and Trend decomposition using Loess) decomposition on a given time series.
+
+    Args:
+        time_series: Array-like object representing the time series data.
+        time_series_length: The length of the processing time series data.
+        time_series_index: Index of the specific time series to decompose within the given array.
+    """
+    log_time_series = np.log(time_serie[:length, index] + 1)
+    res_robust = STL(log_time_series, period=3, robust=True).fit()
+    res_non_robust = STL(log_time_series, period=3, robust=False).fit()
+
+    axs = res_robust.plot().get_axes()
+    components = ["trend", "seasonal", "resid"]
+    for ax, component in zip(axs[1:], components):
+        series = getattr(res_non_robust, component)
+        if component == "resid":
+            ax.plot(series, marker="o", linestyle="none")
+        else:
+            ax.plot(series)
+            if component == "trend":
+                ax.legend(["Robust", "Non-robust"], frameon=False)
+
+    for name, res in [("Non-robust", res_non_robust), ("Robust", res_robust)]:
+        trend_mape = mape(log_time_series, res.trend)
+        trend_u1 = u_1(log_time_series, res.trend)
+        trend_u2 = u_2(log_time_series, res.trend)
+        trend_seasonality_mape = mape(log_time_series, res.trend + res.seasonal)
+        trend_seasonality_u1 = u_1(log_time_series, res.trend + res.seasonal)
+        trend_seasonality_u2 = u_2(log_time_series, res.trend + res.seasonal)
+
+        log_separator()
+        log.info("%s trend MAPE: %s", name, trend_mape)
+        log.info("%s trend U1: %s", name, trend_u1)
+        log.info("%s trend U2: %s", name, trend_u2)
+        log.info("%s trend and seasonality MAPE: %s", name, trend_seasonality_mape)
+        log.info("%s trend and seasonality U1: %s", name, trend_seasonality_u1)
+        log.info("%s trend and seasonality U2: %s", name, trend_seasonality_u2)
+
+    plt.show()
+
+
+def perform_stl_forecasting(time_serie, length, index, steps, is_robust):
+    """
+    Performs STL forecasting on a time series and plots the results.
+
+    Args:
+        time_series: Array-like object representing the time series data.
+        time_series_length: The length of the processing time series data.
+        time_series_index: Index of the time series column.
+        steps: Number of future time steps to forecast.
+        is_robust: Indicates whether to use robust STL decomposition or not.
+    """
+    forecast_type = "Robust" if is_robust else "Non-Robust"
+
+    forecast = (
+        STLForecast(
+            np.log(time_serie[:length, index] + 1),
+            ARIMA,
+            model_kwargs={"order": (2, 1, 0)},
+            period=5,
+            robust=False,
+        )
+        .fit()
+        .forecast(steps)
+    )
+
+    plt.figure(figsize=(10, 6), label=f"STL Forecasting: {forecast_type}")
+    plt.plot(
+        np.log(time_serie[: length + steps, index] + 1),
+        label="Original",
+        color="blue",
+    )
+    plt.plot(
+        range(length, length + steps),
+        forecast,
+        label="Forecast",
+        color="red",
+    )
+
+    plt.xlabel("Time")
+    plt.ylabel("Log Scale")
+    plt.title(f"STL Forecasting: {forecast_type}")
+    plt.legend()
+    plt.show()
+
+
+def main():
+    """
+    Main function for executing the forecasting process.
+    """
+    args = get_cmd_args()
+
+    time_serie_name = get_or_default(args, "--ts-name", "NA_Sales")
+    time_serie_index = int(get_or_default(args, "--ts-index", 0))
+    time_serie_length = int(get_or_default(args, "--ts-length", 200))
+    forecast_steps = int(get_or_default(args, "--forecast-steps", 6))
+
+    data_frame = read_csv_file(args["--path"], columns=[time_serie_name])
+    data_frame_values = data_frame.values
+
+    build_time_series(
+        data_frame=data_frame,
+        length=time_serie_length,
+        index=time_serie_index,
+        name=time_serie_name,
+    )
+
+    register_matplotlib_converters()
+    sns.set_style("darkgrid")
+    plt.rc("figure", figsize=(16, 12))
+    plt.rc("font", size=13)
+
+    log_separator()
+    log.info(
+        "Testing stationarity: Augmented Dickey-Fuller unit root test\n\t%s",
+        adfuller(data_frame_values[:time_serie_length, time_serie_index]),
+    )
+    log.info(
+        "Testing normality: Perform the Jarque-Bera goodness of fit test on sample data\n\t%s",
+        stats.jarque_bera(data_frame_values[:time_serie_length, time_serie_index]),
+    )
+
+    check_stationarity(
+        time_serie=data_frame_values[:time_serie_length, time_serie_index],
+        window_size=12,
+    )
+
+    perform_stl_decomposition(
+        time_serie=data_frame_values,
+        length=time_serie_length,
+        index=time_serie_index,
+    )
+
+    perform_stl_forecasting(
+        time_serie=data_frame_values,
+        length=time_serie_length,
+        index=time_serie_index,
+        steps=forecast_steps,
+        is_robust=True,
+    )
+
+    perform_stl_forecasting(
+        time_serie=data_frame_values,
+        length=time_serie_length,
+        index=time_serie_index,
+        steps=forecast_steps,
+        is_robust=False,
+    )
+
+
+if __name__ == "__main__":
+    main()
